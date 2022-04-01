079f766435aa6dfd61109d2c8e1b1b174ed64294
sumin <lsm001005@naver.com>
2022-04-01 10:28:29 +0900

    Find the index when click a button in the to-do list

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,8 +1,30 @@
-import { IToDo } from "./atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "./atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const { currentTarget: { name },
+        } = event;
+        setToDos((oldToDos) => {
+            const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
+            const oldToDo = oldToDos[targetIndex];
+            const newTodo = { text, id, category: name };
+            console.log(oldToDo, newTodo);
+            return oldToDos;
+        })
+    };
     return (
-        <li>{text}</li>
+        <li>
+            <span>{text}</span>
+            {category !== "TO_DO" &&
+                <button name="TO_DO" onClick={onClick}>To Do</button>}
+            {category !== "DOING" &&
+                <button name="DOING" onClick={onClick}>Doing</button>}
+            {category !== "DONE" &&
+                <button name="DONE" onClick={onClick}>Done</button>}
+        </li>
     );
 }
 export default ToDo;
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,17 +1,18 @@
-import { atom, useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilValue } from "recoil";
 import { toDoState } from "./atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
     const toDos = useRecoilValue(toDoState);
+    console.log(toDos);
     return (
         <div>
             <h1>To Dos</h1>
             <hr />
             <CreateToDo />
             <ul>
-                {toDos.map(toDo => <ToDo {...toDo} />)}
+                {toDos.map(toDo => <ToDo key={toDo.id} {...toDo} />)}
             </ul>
         </div>
     );
